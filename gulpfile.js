f2dc5bc68d9623f499d07dacdbdd596a4ce23609
Michaela-Davis <michaela.delight@gmail.com>
2017-03-12 18:56:20 -0700

    finish 'Using Browersify with Gulp'

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require('gulp');
 
 //more dependencies will be added here.
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
